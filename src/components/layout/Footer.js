03ca228ce8d4489cc7b0fa1b7c35dc0fc7709e63
Fagner Moraes <fagm.carvalho@gmail.com>
2022-07-10 16:25:25 -0300

    Implement react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
+
+import styles from './Footer.module.css'
+
 function Footer(){
     return (
-        <div>
-            <p>rodapé</p>
-        </div>
-    )
+        <footer>          
+        <ul className={styles.social_list}>
+            <li><FaFacebook  /> @fagnermoraes</li>
+            <li><FaTwitter  /> @fagnerkill</li>
+            <li><FaLinkedin  /> @fagnermoraes</li>
+        </ul>      
+        </footer>    )
 }
 
 export default Footer;
